2a9010fba5cbb69994338443c7be73946c33732c
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-09-23 10:42:08 +0200

    created CRUD for Habit model and list view for public habits

--- atomic_habits/views.py
+++ atomic_habits/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from atomic_habits.models import Habit
+from atomic_habits.paginators import HabitPaginator
+from atomic_habits.seriallizers import HabitsSerializer
 
-# Create your views here.
+
+class HabitsViewSet(viewsets.ModelViewSet):
+    """ Класс для механизма CRUD для модели Habit """
+
+    serializer_class = HabitsSerializer
+    queryset = Habit.objects.all()
+    permission_classes = []
+    pagination_class = HabitPaginator
+
+# реализовать регистрацию для привязки пользователя
+    # def perform_create(self, serializer):
+    #     new_habit = serializer.save()
+    #     new_habit.user = self.request.user
+    #     new_habit.save()
+
+
+class PublicHabitsListAPIView(generics.ListAPIView):
+    """ Отдельный клас для выведения списка только публичных привычек """
+    serializer_class = HabitsSerializer
+    queryset = Habit.objects.filter(is_published=True)
+
