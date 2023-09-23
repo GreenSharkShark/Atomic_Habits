c165b6c774c4dd06a27e2fb1521a25bccb09851a
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-09-23 10:42:56 +0200

    registered urls for views

--- atomic_habits/urls.py
+++ atomic_habits/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from atomic_habits.apps import AtomicHabitsConfig
 from atomic_habits.views import *
+from rest_framework.routers import DefaultRouter
 
 
 app_name = AtomicHabitsConfig.name
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'habits', HabitsViewSet, basename='habits')
+
+urlpatterns = [
+    path('habits/public/', PublicHabitsListAPIView.as_view(), name='public_habits'),
+] + router.urls
