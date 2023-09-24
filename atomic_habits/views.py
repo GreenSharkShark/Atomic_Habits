83a8ea9b76225679736709fa548726ac042da242
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-09-24 15:08:36 +0200

    changed objects permissions

--- atomic_habits/views.py
+++ atomic_habits/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, generics
 from atomic_habits.models import Habit
 from atomic_habits.paginators import HabitPaginator
+from atomic_habits.permissions import IsOwner
 from atomic_habits.seriallizers import HabitsSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class HabitsViewSet(viewsets.ModelViewSet):
@@ -9,18 +11,26 @@ class HabitsViewSet(viewsets.ModelViewSet):
 
     serializer_class = HabitsSerializer
     queryset = Habit.objects.all()
-    permission_classes = []
+    permission_classes = [IsAuthenticated, IsOwner]
     pagination_class = HabitPaginator
 
-# реализовать регистрацию для привязки пользователя
-    # def perform_create(self, serializer):
-    #     new_habit = serializer.save()
-    #     new_habit.user = self.request.user
-    #     new_habit.save()
+    def get_queryset(self):
+        """ При GET запросе возвращает список объектов создателем которых является текущий пользователь """
+
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """ Сохраняет текущего пользователя как создателя объекта """
+
+        new_habit = serializer.save()
+        new_habit.user = self.request.user
+        new_habit.save()
 
 
 class PublicHabitsListAPIView(generics.ListAPIView):
     """ Отдельный клас для выведения списка только публичных привычек """
+
     serializer_class = HabitsSerializer
     queryset = Habit.objects.filter(is_published=True)
+    permission_classes = [IsAuthenticated]
 
